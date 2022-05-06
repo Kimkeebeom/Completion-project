04a02cf27c27d1f44f5becb18cff193642d7a587
wisdoom03 <wisdoom03@gmail.com>
2022-05-06 22:40:48 +0900

    pick

--- src/components/units/brand/best/bestItems.presenter.tsx
+++ src/components/units/brand/best/bestItems.presenter.tsx
@@ -1,8 +1,9 @@
+import { useRouter } from "next/router";
 import * as B from "./bestItems.styles";
 
 export default function BestItemsUI(props) {
-  console.log(props.data);
-
+  // console.log(props.data);
+  const router = useRouter();
   return (
     <B.Wrap>
       <h1>BEST</h1>
