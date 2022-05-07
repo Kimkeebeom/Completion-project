78f28e49b9a17193cf40300f8c53379a25467bf9
Kimkeebeom <kkb9102@naver.com>
2022-05-08 02:47:27 +0900

    댓글 수정 / 삭제

--- src/components/units/brand/questionAndAnswer/question/list/questionList.presenter.tsx
+++ src/components/units/brand/questionAndAnswer/question/list/questionList.presenter.tsx
@@ -1,7 +1,5 @@
 import QuestionEdit from "../edit/questionEdit.container";
-import * as L from "./questionList.styles";
 export default function QuestionListUI(props) {
-  //   console.log(props.data?.fetchUseditemQuestions);
   return (
     <div>
       <QuestionEdit key={props.el._id} el={props.el} />
